14acd0c0f6aeda7e17b6b08931f9b4d2dbab4750
EstanislauFilho <estanislau.sena@gmail.com>
2022-09-25 20:23:39 -0300

    Adicionado instrução para ignorar os warnings gerados pelo tensorflow.

--- settings/setup.py
+++ settings/setup.py
@@ -1,5 +1,7 @@
 """ Script para configurações da aplicação
 """
+import os 
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
 
 DATASET_PATH = "./dataset/defect_and_no_defect.csv"
 IMAGES_PATH = "./images/"
