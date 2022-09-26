1c456a645879db17d412a49aead26cbb7392ecd7
EstanislauFilho <estanislau.sena@gmail.com>
2022-09-26 20:47:31 -0300

    Visualização do dataframe de contagem de erros por imagem.

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import matplotlib.pyplot as plt
 from skimage import io
 from libraries import etl
 from settings import setup
+from libraries import graphics
 from tensorflow.keras import layers, optimizers
 from tensorflow.keras.applications import ResNet50
 from tensorflow.keras.models import Model, load_model
@@ -22,14 +23,19 @@ class Main:
         pass
 
     def main(self):
-        dataset = etl.load_dataset(setup.DATASET_PATH)
+        steel_plates_dataset = etl.load_dataset(setup.DATASET_PATH)
 
         defective_steel_plates_dataset = etl.load_dataset(setup.DATASET_DEFECT_TRAIN)
 
         quantity_defective_steel_plates = \
-            (defective_steel_plates_dataset.shape[0] / dataset.shape[0]) * 100
+            (defective_steel_plates_dataset.shape[0] / steel_plates_dataset.shape[0]) * 100
             
-        print(quantity_defective_steel_plates)
+        defective_steel_plates_summary = etl.count_of_defect_types_per_image(defective_steel_plates_dataset)
+
+        print(defective_steel_plates_summary)
+
+        graphics.relationship_between_defects_and_no_defects(steel_plates_dataset, display=False)
+        graphics.relationship_between_types_of_defects(defective_steel_plates_dataset, display=False)
 
 if __name__ == "__main__":
     run = Main()
