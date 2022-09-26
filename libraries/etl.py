3a250ffe1be68dd9235a178f80253ab4ac121dd6
EstanislauFilho <estanislau.sena@gmail.com>
2022-09-26 20:38:54 -0300

    Implementado função para contar a quantidade de defeitos por imagem.

--- libraries/etl.py
+++ libraries/etl.py
@@ -11,3 +11,22 @@ def load_dataset(dataset_path: str) -> "pd.core.frame.DataFrame":
         pd.core.frame.DataFrame: DataFrame com os dados do dataset.
     """
     return pd.read_csv(dataset_path)
+
+
+def count_of_defect_types_per_image(dataset: "pd.core.frame.DataFrame") \
+                                    -> "pd.core.frame.DataFrame":
+    """ Função responsável por analisar o dataset de placas de aço com defeito 
+    e contar a quantidade de defeitos presente em cada imagem.
+
+    Args:
+        dataset (pd.core.frame.DataFrame): Dataset a ser analisado.
+    Returns:
+        dataset (pd.core.frame.DataFrame): Dataset com nova coluna indicando a
+        quantidade defeitos encontrados para cada imagem.
+    """
+    # adiciona um nova coluna no final do dataframe
+    dataset['mask'] = dataset['ClassId'].map(lambda x: 1)
+    
+    # faz o agrupamento das imagens repetidas somando o atributo mask,
+    # sendo possível assim contar a quantidade defeitos por imagem.
+    return dataset.groupby(['ImageId'])['mask'].sum()
