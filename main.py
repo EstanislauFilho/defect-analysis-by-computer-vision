249e7818ccf002f9e7e44f7c947347b5f9738946
EstanislauFilho <estanislau.sena@gmail.com>
2022-09-27 21:39:28 -0300

    Visualização da imagem em formato de mascara.

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from skimage import io
 from libraries import etl
 from settings import setup
 from libraries import graphics
+from libraries import image_functions
 from tensorflow.keras import layers, optimizers
 from tensorflow.keras.applications import ResNet50
 from tensorflow.keras.models import Model, load_model
@@ -30,20 +31,32 @@ class Main:
         # quantity_defective_steel_plates = \
         #     (defective_steel_plates_dataset.shape[0] / steel_plates_dataset.shape[0]) * 100
             
-        defective_steel_plates_summary = etl.count_of_defect_types_per_image(defective_steel_plates_dataset)
-
-        print(defective_steel_plates_summary)
+        defect_type = etl.count_of_defect_types_per_image(defective_steel_plates_dataset)
 
         # graphics.relationship_between_defects_and_no_defects(steel_plates_dataset, display=False)
         # graphics.relationship_between_types_of_defects(defective_steel_plates_dataset, display=False)
         # graphics.count_of_defect_types_per_image(defective_steel_plates_summary, display=True)
 
-        for i in range(2):
-            img = io.imread(os.path.join("./images/train_images/", defective_steel_plates_dataset['ImageId'][i]))
-            plt.figure()
-            plt.title(defective_steel_plates_dataset['ClassId'][i])
-            plt.imshow(img)
-            plt.show()
+        # print(defective_steel_plates_dataset['EncodedPixels'])
+
+        img = io.imread(os.path.join("./images/train_images/", defective_steel_plates_dataset['ImageId'][20]))
+        # função que cria uma macara com o mesmo tamanho da altura e largura da imagem
+        # RUN LENGTH ENCODING (RLE) é uma técnica para compressão de dados que armazena 
+        # sequências que contém dados consecutivos (combinação em um só valor)
+        # Considerando que temos uma imagem com texto preto em fundo branco
+        # Em uma image 800x600 tem se 480.000 valores e com o método RLE tem uma sequencia
+        # bem reduzida o que ocupa menos espaço para armazenamento. 
+        mask = image_functions.rle2mask(defective_steel_plates_dataset['EncodedPixels'][20], img.shape[0], img.shape[1])       
+        plt.figure()
+        plt.title(defective_steel_plates_dataset['ClassId'][20])
+        plt.imshow(mask)
+        plt.show()
+
+
+            # plt.figure()
+            # plt.title(defective_steel_plates_dataset['ClassId'][i])
+            # plt.imshow(img)
+            # plt.show()
 
 if __name__ == "__main__":
     run = Main()
