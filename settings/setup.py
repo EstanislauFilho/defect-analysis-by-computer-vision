4f570bb4dfb7fb7e5d59e8596d5be6c7cf91d428
EstanislauFilho <estanislau.sena@gmail.com>
2022-09-29 17:06:41 -0300

    Especificação do caminho do modelo da rede neural resnet e dos seus respectivos pesos.

--- settings/setup.py
+++ settings/setup.py
@@ -6,3 +6,6 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 DATASET_PATH = "./dataset/defect_and_no_defect.csv"
 DATASET_DEFECT_TRAIN = "./dataset/train.csv"
 IMAGES_PATH = "./images/"
+
+RESNET_MODEL_PATH = "./models/resnet-classifier-model.json"
+RESNET_WEIGHTS_PATH = "./models/weights.hdf5"
